f4936c6fe01bfe29b0db9750e58838546d0ccf6b
Vidura Hemawansa <vishwanath.wgv2h@gmail.com>
2023-02-25 09:07:24 +0530

    added procfile

--- src/app/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/task-manager'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/task-manager/index.html'));});
-app.listen(process.env.PORT || 8080);
